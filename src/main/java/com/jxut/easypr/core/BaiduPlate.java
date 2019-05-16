a523bd23738c611bc3922274a714258907c4c6ce
JXUT_Chen <1170712462@qq.com>
2019-05-16 10:05:12 +0800

    新增百度云识别api

---
+++ src/main/java/com/jxut/easypr/core/BaiduPlate.java
@@ -0,0 +1,37 @@
+package com.jxut.easypr.core;
+import com.baidu.aip.ocr.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * @Author JXUT CXY
+ * @Description
+ * @Date: created in 9:16 2019/5/16
+ */
+public class BaiduPlate {
+    public static final String APP_ID = "16262933";
+    public static final String API_KEY = "y60u20GyD1fq1hF31kcG8Riv";
+    public static final String SECRET_KEY = "fLlSjkR42o5o1UrHow5G00nHSEEkyhp4";
+
+    public String plate(String image) {
+
+        AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("multi_detect", "true");
+
+
+        // 参数为本地图片路径
+        JSONObject res = client.plateLicense(image, options);
+        JSONArray result=res.getJSONArray("words_result");
+        JSONObject result1=result.getJSONObject(0);
+        String plate=result1.getString("number");
+
+        return plate
+
+
+    }
+}
